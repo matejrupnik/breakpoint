6f87d8d12ce431a992fca88ba7fdfe5f66a08f10
matejrupnik <matej.rupnik02@gmail.com>
2025-04-13 02:18:30 +0200

    deploy

--- backend/database/seeders/1_insert_surface_readings.up.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO surface_readings (car_id, surface_reading, location)
-SELECT
-    (SELECT device_fingerprint FROM cars ORDER BY RANDOM() LIMIT 1),
-    ROUND((RANDOM() * 9.8 + 0.1)::numeric, 2),
-    ST_MAKEPOINT(
-            14.50 + (random() * 0.1 - 0.05),
-            46.05 + (random() * 0.1 - 0.05)
-    )::GEOGRAPHY
-FROM GENERATE_SERIES(1, 1000);
