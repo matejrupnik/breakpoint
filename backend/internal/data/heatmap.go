912b8a6bc37b3772f7161d1328221ab6e592e77f
matejrupnik <matej.rupnik02@gmail.com>
2025-04-13 04:46:18 +0200

    deploy

--- backend/internal/data/heatmap.go
+++ backend/internal/data/heatmap.go
@@ -8,7 +8,11 @@ import (
 )
 
 type Heatmap struct {
-	Surfaces []Surface `json:"surfaces"`
+	Idles    []Surface `json:"idle"`
+	Asphalts []Surface `json:"asphalt"`
+	Gravels  []Surface `json:"gravel"`
+	Roughs   []Surface `json:"rough"`
+	Potholes []Surface `json:"pothole"`
 }
 
 type HeatmapModel struct {
@@ -41,7 +45,23 @@ func (m HeatmapModel) GetByLocation(longitude float64, latitude float64) (*Heatm
 			return nil, err
 		}
 
-		heatmap.Surfaces = append(heatmap.Surfaces, surface)
+		switch surface.Type {
+		case "fc005337-7691-4a26-8d48-3ff31eb767ce":
+			heatmap.Idles = append(heatmap.Idles, surface)
+			break
+		case "7bf36d1a-70dc-4303-916c-445b0f251d5f":
+			heatmap.Asphalts = append(heatmap.Asphalts, surface)
+			break
+		case "898a60b2-9eb4-4fb9-a73f-751a84362641":
+			heatmap.Gravels = append(heatmap.Gravels, surface)
+			break
+		case "a7cf5742-c888-4812-b867-e44f680414ed":
+			heatmap.Roughs = append(heatmap.Roughs, surface)
+			break
+		case "ab6646f7-12bf-47dc-94a1-ff84b1c1833f":
+			heatmap.Potholes = append(heatmap.Potholes, surface)
+			break
+		}
 	}
 
 	if err := rows.Err(); err != nil {
